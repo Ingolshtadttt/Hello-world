6e339f6c4ea8b0a767b48d430e95a5616a82c891
Ingolshtadttt <114305035+Ingolshtadttt@users.noreply.github.com>
2022-10-31 16:53:25 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args) throws IOException {
+        var students = new Students();
+        var st = students.getStudent("Уткин Павел");
+        System.out.println(st);
+        System.out.println();
+        System.out.println(st.getFinalScores());
+        System.out.println();
+        System.out.println(st.getResultModules());
+    }
 }
