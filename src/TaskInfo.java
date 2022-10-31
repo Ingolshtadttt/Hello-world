c8b5017856367171dd9ceed2e7b7080d32e9bd4b
Ingolshtadttt <114305035+Ingolshtadttt@users.noreply.github.com>
2022-10-31 16:57:06 +0500

    Update TaskInfo.java

--- src/TaskInfo.java
+++ src/TaskInfo.java
@@ -1,2 +1,21 @@
 public class TaskInfo {
+    private final String taskName;
+    private final float maxScore;
+    private final float pointsScored;
+
+    public TaskInfo(String taskName, float maxScore, float pointsScored){
+        this.taskName = taskName;
+        this.maxScore = maxScore;
+        this.pointsScored = pointsScored;
+    }
+
+    public String getTaskName(){ 
+        return taskName;
+    }
+    public float getMaxScore(){ 
+        return maxScore;
+    }
+    public float getPointsScored(){
+        return pointsScored; 
+    }
 }
