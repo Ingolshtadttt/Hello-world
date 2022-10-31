d69b7b52b509c7a0606436c1827de330b0ed5c95
Ingolshtadttt <114305035+Ingolshtadttt@users.noreply.github.com>
2022-10-31 16:52:52 +0500

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,69 @@
+import java.util.ArrayList;
+
 public class Course {
+    private final ArrayList<Module> modules = new ArrayList<Module>();
+    private final float activityMaxScore;
+    private final float exercisesMaxScore;
+    private final float homeworkMaxScore;
+    private final float semMaxScore;
+
+    public Course(float activityMaxScore, float exercisesMaxScore, float homeworkMaxScore, float semMaxScore){
+        this.activityMaxScore = activityMaxScore;
+        this.exercisesMaxScore = exercisesMaxScore;
+        this.homeworkMaxScore = homeworkMaxScore;
+        this.semMaxScore = semMaxScore;
+    }
+
+    public float getActivityMaxScore(){
+        return activityMaxScore;
+    }
+
+    public float getExercisesMaxScore(){
+        return exercisesMaxScore;
+    }
+
+    public float getHomeworkMaxScore(){
+        return homeworkMaxScore;
+    }
+
+    public float getSemMaxScore(){
+        return semMaxScore;
+    }
+
+    public void addModule(Module module){
+        modules.add(module);
+    }
+
+    public ArrayList<Module> getModules(){
+        return modules;
+    }
+
+    public float getExercisesScore(){
+        var score = 0f;
+        for(var i: modules)
+            score += i.getExercisesScore();
+        return score;
+    }
+
+    public float getHomeworkScore(){
+        var score = 0f;
+        for(var i: modules)
+            score += i.getHomeworkScore();
+        return score;
+    }
+
+    public float getActivityScore(){
+        var score = 0f;
+        for(var i: modules)
+            score += i.getActivityScore();
+        return score;
+    }
+
+    public float getSemScore(){
+        var score = 0f;
+        for(var i: modules)
+            score += i.getSemScore();
+        return score;
+    }
+
 }
