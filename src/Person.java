8778ddcf710d01027e2492330f48b7e013624c66
Ingolshtadttt <114305035+Ingolshtadttt@users.noreply.github.com>
2022-10-31 16:53:53 +0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,24 @@
 public class Person {
+    private final String surnameName;
+
+    enum Gender{
+        Male,
+        Female
+    }
+
+    public Person(String surnameName){
+        this.surnameName = surnameName;
+    }
+
+    public String getSurname() {
+        return "";
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public String getSurnameName() {
+        return surnameName;
+    }
 }
